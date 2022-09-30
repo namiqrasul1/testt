1bbd9355e47c622f3faf2658492b65401ef96fe5
Rasullu Namiq <n.rasullu@itstep.edu.az>
2022-09-30 19:49:08 +0400

    hakuna

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -4,4 +4,17 @@ using namespace std;
 
 void main() {
 	cout << "salam!\n";
+	// code
+	// code
+	// code
+	// code
+	// code
+	// code
+	// code
+	// code
+	// code
+	// code
+	// code
+	// code
+	// code
 }
